c04130b5cc9f15dd336d414f771032f6a6b1b8a9
ckjellstrand <bkjell@gmail.com>
2022-07-15 12:33:46 -0400

    Added in a message for lowrate downlinking both to test and to remind groups to fill in their status summary'

--- skywinder/communication/streamlined_communicator.py
+++ skywinder/communication/streamlined_communicator.py
@@ -336,7 +336,8 @@ class Communicator(GlobalConfiguration):
     ### The following two functions respond to SIP requests
     def respond_to_science_data_request(self, lowrate_index):
         logger.debug("Science data request received from %s." % self.lowrate_uplinks[lowrate_index].name)
-        #self.lowrate_downlinks[lowrate_index].send(summary)
+        summary = b'put your custom status summary here'
+        self.lowrate_downlinks[lowrate_index].send(summary)
         # Experiment groups should fill in their own status here.
 
     def process_science_command_packet(self, msg, lowrate_index):
