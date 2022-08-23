7a2bcf49b643d0ab94e44eef8338b33a9fd0abb8
ckjellstrand <bkjell@gmail.com>
2022-08-23 11:14:08 -0400

    Some groups want to rewrite received data to disk by source with payloads included.

---
+++ scripts/python/write_and_sort_files_by_source.py
@@ -0,0 +1,10 @@
+import glob
+import os
+from skywinder.communication import file_format_classes
+
+
+def sort_and_write(input_dir, output_base_dir):
+    for fn in glob.glob(input_dir):
+        myfile = file_format_classes.GeneralFile.from_file(fn)
+        out_fn = os.path.join(output_base_dir, myfile.camera_id, myfile.filename)
+        myfile.write_payload_to_file(out_fn)
