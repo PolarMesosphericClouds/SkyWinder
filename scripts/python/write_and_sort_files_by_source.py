b58757ec49b94e57049ddc2e3c29fca53270ec2a
ckjellstrand <bkjell@gmail.com>
2022-08-23 13:17:46 -0400

    typing error

--- scripts/python/write_and_sort_files_by_source.py
+++ scripts/python/write_and_sort_files_by_source.py
@@ -6,5 +6,5 @@ from skywinder.communication import file_format_classes
 def sort_and_write(input_dir, output_base_dir):
     for fn in glob.glob(input_dir):
         myfile = file_format_classes.GeneralFile.from_file(fn)
-        out_fn = os.path.join(output_base_dir, myfile.camera_id, myfile.filename)
+        out_fn = os.path.join(output_base_dir, str(myfile.camera_id), myfile.filename)
         myfile.write_payload_to_file(out_fn)
